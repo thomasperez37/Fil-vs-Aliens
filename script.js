0576ce99c8efb34c1bfda2cda1b850ce6fcba0a4
Thomas Perez <thomasperez@uagateway.org>
2017-05-17 20:38:05 +0000

    I created controls for the game

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    
+    function checkCollision() {
+        /*var leftBoundsRightSide = $("#left_bound").offset().left + width;*/
+    }
+    function spawnBoxSprite() {
+        
+    }
+    $("body").keydown(function(event) {
+        if (event.which === 37) {
+            var left = document.body.clientWidth;
+            console.log(left);
+            $("#fil").css("left", $("#fil").offset().left + "%");
+        } else if (event.which === 39) {
+            var right = $("#fil").offset().left + 10;
+            $("#fil").css("left", right + "%");
+        } else {
+            return;
+        }
+    });
 });
