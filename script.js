aac44009e4a9ebab838ff62d42f87a62ab36f996
Thomas Perez <thomasperez@uagateway.org>
2017-11-08 23:03:35 +0000

    Unknown changes

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
     function checkCollision() {
         /*var leftBoundsRightSide = $("#left_bound").offset().left + width;*/
     }
     function spawnBoxSprite() {
+        $("body").append("<div class='box_alien'><img src='images/Small_Box_sprite_015 copy.png'></div>");
+        var randomLocation = getRandomIntInclusive(25,75);
+        randomLocation = String(randomLocation);
+        
         
     }
     $("body").keydown(function(event) {
         if (event.which === 37) {
-            var left = document.body.clientWidth;
-            console.log(left);
-            $("#fil").css("left", $("#fil").offset().left + "%");
+            $("#fil").css("left", $("#fil").offset().left - 10);
         } else if (event.which === 39) {
-            var right = $("#fil").offset().left + 10;
-            $("#fil").css("left", right + "%");
+            $("#fil").css("left", $("#fil").offset().left + 10);
         } else {
             return;
         }
     });
+    spawnBoxSprite();
 });
